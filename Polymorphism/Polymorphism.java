12d308bd20e4576b1465df4d1436640f0fa81af0
unknown <Arif>
2020-07-15 21:04:10 -0400

    Polymorphism File Added

---
+++ Polymorphism/Polymorphism.java
@@ -0,0 +1,19 @@
+package Polymorphism;
+
+public class Polymorphism {
+
+	public static void print (){
+		
+		System.out.println("Hi");
+	
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+      print();
+
+	}
+
+}
